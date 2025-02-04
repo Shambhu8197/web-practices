8ed393782bf4b4594b7372bfda82a458d60b9e28
shambhu R <shambhukumarr8197@gmail.com>
2025-02-04 10:46:25 +0530

    Add comments to clarify input handling and operation logic in faulty calculator

--- javascript/Faulty calculator/app.js
+++ javascript/Faulty calculator/app.js
@@ -1,15 +1,17 @@
 let random = Math.random()
+//this line is use to get the input 
 let a = prompt("enter the first number")
 let b = prompt("enter the operation")
 let c = prompt("enter the third number")
 
+//this is used for operation
 let obj ={
     "+":"-",
     "*":"+",
     "-": "/",
     "/":"*",
 }
-
+// this to perform the condition
 if (random>0.1){
     console.log(`the result is ${a} ${b} ${c}`)
     alert(`the result is ${eval(`${a} ${b} ${c}`)}`)
